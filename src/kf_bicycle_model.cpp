620215ce3e8a4f99292a4df4f544536e70e9f282
girish-radhakrishnan-250894 <143761842+girish-radhakrishnan-250894@users.noreply.github.com>
2023-12-08 06:01:12 -0800

    added kalman_filter files
    added kalman_filter files

--- src/kf_bicycle_model.cpp
+++ src/kf_bicycle_model.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "kf_bicycle_model.h"
+
+kf_bicycle_model::kf_bicycle_model(vehicle_model_fw_simplified &_vehicle_model) : vehicle_model(_vehicle_model) {}
+
+void kf_bicycle_model::operator() ( const state_type &x , state_type &dxdt , const double  t  )
+{
+    // Interpolating the steering input
+    double delta_c{0};
+
+    if (t > 4)
+    {
+        delta_c = 0.05;
+    }
+
+    // Initializing a driving torque input
+    double m_d_c{0};
+
+    Eigen::VectorXd q = Eigen::VectorXd::Zero(28);
+
+    for (int i = 0; i < x.size(); ++i) {
+        q(i) = x[i];
+    }
+
+    Eigen::VectorXd Qdot = vehicle_model.solve(q,delta_c, m_d_c);
+
+    for (int i=0; i < Qdot.size(); ++i)
+    {
+        dxdt[i] = (Qdot(i));
+    }
+
+
+
+
+
+}
--- src/kf_bicycle_model.h
+++ src/kf_bicycle_model.h
@@ -5,9 +5,20 @@
 #ifndef KALMAN_FILTER_FOUR_WHEEL_MODEL_KF_BICYCLE_MODEL_H
 #define KALMAN_FILTER_FOUR_WHEEL_MODEL_KF_BICYCLE_MODEL_H
 
+#include "eigen3/Eigen/Dense"
+#include "../submodules/sensor_data/vehicle_model_fw_simplified.h"
+
+typedef std::vector<double> state_type;
+
 
 class kf_bicycle_model {
 
+    vehicle_model_fw_simplified vehicle_model;
+
+public:
+    kf_bicycle_model(vehicle_model_fw_simplified &_vehicle_model);
+
+    void operator() (const state_type &x, state_type  &dxdt, const double t);
 };
 
 
